144c48765dd8a6ca27719f602ffc9126958faf4a
Sunny <sunnygb.8@gmail.com>
2022-02-16 22:58:24 +0000

    Add about page using tailwind css

---
+++ src/pages/about.js
@@ -0,0 +1,11 @@
+import * as React from "react"
+
+export default function AboutPage() {
+    return (
+      <div class="p-6 max-w-sm mx-auto mt-6 bg-white rounded-xl shadow-lg flex items-center space-x-4">
+        <h1 className="text-3xl font-bold underline">
+          Hello Tailwind on Gatsby!
+        </h1>
+      </div>
+    )
+  }
